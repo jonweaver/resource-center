b4f85d70100fa175a7b9c33f1cc9b3ed8649d952
Jon Weaver <jw7694@gmail.com>
2019-05-17 23:26:46 -0400

    init repo

---
+++ CBC.ResourceCenter/CBC.ResourceCenter/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CBC.ResourceCenter.Models;
+
+namespace CBC.ResourceCenter.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
